08931fbd4e5cb04b9ee38a9df28b4cf52a056958
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-06-02 16:40:55 +0530

    1st commit

---
+++ new.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return fact(n - 1) * n;
+}
+
+int Ifact(int n)
+{
+    int f = 1;
+    int i;
+    for (int i = 0; i < n; i++)
+    {
+        f = f*i;
+    }
+    return f;
+}
+
+int main()
+{
+    int r;
+    r = Ifact(5);
+    cout << r << endl;
+    return 0;
+}
